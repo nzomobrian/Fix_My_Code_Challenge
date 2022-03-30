a42ab6310071646760211484da187032c2c0d026
pericles001 <periclesadjovi@outlook.fr>
2022-03-30 22:58:56 +0100

    Updated file with renaming

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Square module"""
+""" Square module. """
 
 
-class square():
-    """Square class"""
+class Square():
+    """ Square class. """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """"Initialize class"""
+        """ Square class initialized """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,19 +16,18 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def perimeter_of_my_square(self):
-        """Perimeter of the square"""
+    def permiter_of_my_square(self):
+        """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """Return string representation of the square"""
+        """ String representation of the square instance """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """Main function"""
-
-    s = square(width=12, height=9)
+    """ Create Square instance """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.permiter_of_my_square())
