9fc1ba436cff30e37082ec68dc0a45ab2ea61161
pericles001 <periclesadjovi@outlook.fr>
2022-03-30 02:38:21 +0100

    Update file methods names

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -13,11 +13,11 @@ class square():
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_square(self):
+    def area(self):
         """ Area of the square """
         return self.width * self.height
 
-    def perimeter_of_my_square(self):
+    def perimeter(self):
         """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
@@ -29,5 +29,5 @@ if __name__ == "__main__":
 
     s = square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.area())
+    print(s.perimeter())
