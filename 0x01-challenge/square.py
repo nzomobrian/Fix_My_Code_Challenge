132cd37d1e7bd1e22c34ee75dddfe325bb8942ff
pericles001 <periclesadjovi@outlook.fr>
2022-03-30 02:46:34 +0100

    Review code format

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
 """Square module"""
 
+
 class square():
     """Square class"""
-    
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
-        """ Initialize the square """
+        """"Initialize class"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area(self):
+    def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.width * self.width
 
-    def perimeter(self):
-        """ Perimeter of the square """
+    def perimeter_of_my_square(self):
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ Print the square """
+        """Return string representation of the square"""
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
+    """Main function"""
 
     s = square(width=12, height=9)
     print(s)
-    print(s.area())
-    print(s.perimeter())
+    print(s.area_of_my_square())
+    print(s.perimeter_of_my_square())
